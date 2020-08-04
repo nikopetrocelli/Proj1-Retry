deb5489d6ce3cc56d02d12572cca4181eeefe5b5
niko petrocelli <nap335@gmail.com>
2020-08-04 19:18:34 -0400

    fdsaf

--- myshell.c
+++ myshell.c
@@ -182,7 +182,7 @@ int sh_execute(char **args){
                 int i = 3 ;
                 //Concatonate the arguments to create the message
                 while(args[i] != NULL){
-                    strcat(buffer, args[i]);
+                    strcat(buffer, args[i] + ' ');
                     //add space between words
                    // strcat(buffer, ' ');
                     i++;
